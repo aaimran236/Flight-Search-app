98b21c873510dfed478857e7902c515d7adbc2f0
aaimran236 <ai2015236@gmail.com>
2025-11-28 22:10:13 +0600

    Create Room entities

---
+++ app/src/main/java/com/example/flightsearch/data/Airport.kt
@@ -0,0 +1,21 @@
+package com.example.flightsearch.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "airport")
+data class Airport (
+    @PrimaryKey
+    val id: Int,
+
+    @ColumnInfo(name="iata_code")
+    val iATACode: String,
+
+    @ColumnInfo(name="name")
+    val fullAirportName: String,
+
+    @ColumnInfo(name="passengers")
+    val numberOfPassengersPerYear: Int
+
+)
