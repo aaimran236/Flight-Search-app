b90e17f35f0053d212e67cd204245668291d4916
aaimran236 <ai2015236@gmail.com>
2025-12-12 15:27:36 +0600

    Added license for Flight Search App

--- app/src/main/java/com/example/flightsearch/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/flightsearch/ui/home/HomeViewModel.kt
@@ -1,6 +1,20 @@
+/*
+Copyright (C) 2025 aaimran236
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package com.example.flightsearch.ui.home
 
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -12,7 +26,6 @@ import com.example.flightsearch.data.FavoriteAirport
 import com.example.flightsearch.data.Route
 import com.example.flightsearch.data.UserSearchRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
